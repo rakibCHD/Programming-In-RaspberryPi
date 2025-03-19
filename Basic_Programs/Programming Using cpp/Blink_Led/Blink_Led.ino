d2d5c891ca1c0d12737e6f4a53b0e60aeb4dfa96
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-03-19 16:28:11 +0600

    Update Blink_Led.ino

--- Basic_Programs/Programming Using cpp/Blink_Led/Blink_Led.ino
+++ Basic_Programs/Programming Using cpp/Blink_Led/Blink_Led.ino
@@ -1,4 +1,13 @@
-//On Board Built-in Green Led connected with pin 25.
+/*
+-->Board Manager Url:https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+-->Board: "Raspberry Pi Pico"
+-->Flash Size: "2MB (no FS)"
+-->CPU Speed: "125 MHz"
+-->Debug Port: "Disabled"
+-->Debug Level: "None"
+-->Port: First time no need, After Upload a Sketch ,Connect the port.
+-->On Board Built-in Green Led connected with pin 25.
+*/
 
 int led = 25;
 
